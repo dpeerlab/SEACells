4514264b9a86baa56a7c631921b3a1ce42e6784b
Sitara Persad <sitarapersad@harrisons-mbp.myfiosgateway.com>
2021-11-04 16:35:44 -0400

    Removed extra folders

--- build/lib/SEACells/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import core
-from . import preprocess
-from . import utils
-from . import plot
-from .version import __version__
--- build/lib/SEACells/version.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.0.0"
-__author__ = "Manu Setty, Zi-Ning Choo, Sitara Persad"
-__author_email__ = "scp2152@columbia.edu"
