52120cb52156845695ed866edb5949cc6605c162
Sitara Persad <sitarapersad@sitaras-mbp.myfiosgateway.com>
2023-04-14 01:04:41 -0400

    Adding sparse assignment matrices and refactoring CPU and GPU code

--- SEACells/version.py
+++ SEACells/version.py
@@ -1,4 +1,4 @@
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __author__ = "Manu Setty, Zi-Ning Choo, Sitara Persad"
 __author_email__ = "scp2152@columbia.edu"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="SEACells",
-    version="0.3.2",
+    version="0.3.3",
     author="Pe'er Lab",
     author_email="scp2152@columbia.edu",
     description=" ",
